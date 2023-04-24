e6a3ed7fe00133c1236ce0c8711477015dd8742f
Wettergrund <jonas.wettergrund@gmail.com>
2023-04-24 14:07:47 +0200

    Fetch API key from appsettings

--- MovieAPI/Models/TMDBRepository.cs
+++ MovieAPI/Models/TMDBRepository.cs
@@ -1,15 +1,38 @@
-﻿using MovieAPI.connection;
+﻿using Microsoft.IdentityModel.Tokens;
+using MovieAPI.connection;
 using Newtonsoft.Json;
 
 namespace MovieAPI.Models
 {
     public class TMDBRepository : ITMDBRepository
     {
+        private string key()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("./appsettings.json", optional: true, reloadOnChange: true);
+
+            var configuration = configurationBuilder.Build();
+
+            string? result = configuration["key"];
+
+            if (result.IsNullOrEmpty())
+            {
+                return "No key";
+            }
+            else
+            {
+
+                return result;
+            }
+
+        }
+
         public TMDBRepository() { }
 
         public async Task<Result> GetByTitle(string title)
         {
-            string URL = $"https://api.themoviedb.org/3/search/movie?api_key=2b7d5bf25d89ca81c83f8d6a2ac12244&language=en-US&query={title}&include_adult=false";
+            string URL = $"https://api.themoviedb.org/3/search/movie?api_key={key()}44&language=en-US&query={title}&include_adult=false";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
@@ -25,7 +48,7 @@ namespace MovieAPI.Models
 
         public async Task<TMDBMovie> GetByID(int id)
         {
-            string URL = $"https://api.themoviedb.org/3/movie/{id}?api_key=2b7d5bf25d89ca81c83f8d6a2ac12244&language=en-US";
+            string URL = $"https://api.themoviedb.org/3/movie/{id}?api_key={key()}44&language=en-US";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
@@ -41,7 +64,7 @@ namespace MovieAPI.Models
 
         public async Task<Result> GetByGenres(string genres)
         {
-            string URL = $"https://api.themoviedb.org/3/discover/movie?api_key=2b7d5bf25d89ca81c83f8d6a2ac12244&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={genres}&with_watch_monetization_types=flatrate";
+            string URL = $"https://api.themoviedb.org/3/discover/movie?api_key={key()}44&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={genres}&with_watch_monetization_types=flatrate";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
