bd8384fc9cb4025bd44ed1ce3ad54acbd2999ade
Wettergrund <Jonas.wettergrund@gmail.com>
2023-05-07 20:00:03 +0200

    Incorrect path to TMDb API

--- MovieAPI/Models/TMDBRepository.cs
+++ MovieAPI/Models/TMDBRepository.cs
@@ -6,7 +6,7 @@ namespace MovieAPI.Models
 {
     public class TMDBRepository : ITMDBRepository
     {
-        private string key()
+        private string Key()
         {
             var configurationBuilder = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
@@ -32,7 +32,9 @@ namespace MovieAPI.Models
 
         public async Task<Result> GetByTitle(string title)
         {
-            string URL = $"https://api.themoviedb.org/3/search/movie?api_key={key()}44&language=en-US&query={title}&include_adult=false";
+            string URL = $"https://api.themoviedb.org/3/search/movie?api_key={Key()}&language=en-US&query={title}&include_adult=false";
+
+            //https://api.themoviedb.org/3/search/movie?api_key=2cb182b792525c9bcfd1cf968eb35326&language=en-US&query=Titanic&page=1&include_adult=false
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
@@ -48,7 +50,7 @@ namespace MovieAPI.Models
 
         public async Task<TMDBMovie> GetByID(int id)
         {
-            string URL = $"https://api.themoviedb.org/3/movie/{id}?api_key={key()}44&language=en-US";
+            string URL = $"https://api.themoviedb.org/3/movie/{id}?api_key={Key()}&language=en-US";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
@@ -64,7 +66,7 @@ namespace MovieAPI.Models
 
         public async Task<Result> GetByGenres(string genres)
         {
-            string URL = $"https://api.themoviedb.org/3/discover/movie?api_key={key()}44&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={genres}&with_watch_monetization_types=flatrate";
+            string URL = $"https://api.themoviedb.org/3/discover/movie?api_key={Key()}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres={genres}&with_watch_monetization_types=flatrate";
             using (var client = new HttpClient())
             {
                 var response = await client.GetAsync(URL);
--- MovieAPI/Program.cs
+++ MovieAPI/Program.cs
@@ -14,16 +14,30 @@ namespace MovieAPI
 
             // Add services to the container.
             builder.Services.AddAuthorization();
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+            });
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+
             var app = builder.Build();
+
+            app.UseCors();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
+
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
